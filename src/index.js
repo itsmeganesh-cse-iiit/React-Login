318494aa087ca69f6ddeff6bd4f367d5fc04bac3
itsmeganesh-cse-iiit <itsmeganeshcse@gmail.com>
2019-07-29 18:17:04 +0530

    styled  and React lazy and suspence added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Routing from './components/Routing/'
+import styled from 'styled-components'
+const { lazy, Suspense } = React;
 
-ReactDOM.render(<Routing/>, document.getElementById('root'));
+const Lazy = lazy(() => new Promise(resolve => {
+  setTimeout(() => {
+    resolve({ default: () => <Routing /> });
+  }, 3000);
+}));
+  
+const LoadingBar = styled.div`
+margin-top:20%;
+margin-left:45%;
+font-weight:bold;
+font-size:30px;
+
+`
+
+const LazyApp=()=>{
+return(
+  <Suspense fallback={<LoadingBar>Loading...</LoadingBar>}>
+    <Lazy/>
+  </Suspense>
+)
+}
+
+ReactDOM.render(<LazyApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
